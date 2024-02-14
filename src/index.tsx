f9fef7c7df9290e743661a5cfff6cab16a4305d5
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-13 21:36:14 -0600

    simple app created

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <h1>Hello world</h1>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
