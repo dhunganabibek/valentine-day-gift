7e68a4ecb94d9aa67edb4b9ecc4ff856095729bb
Bibek Dhungana <dhunganabibek2016@gmail.com>
2024-02-13 22:06:20 -0600

    tailwind setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,11 @@
+import './App.css';
+
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className='bg-red-500'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
